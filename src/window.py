b191f504391f80f6362656cb31a19ed24512fc19
enprogames <e_posner@outlook.com>
2021-07-27 19:02:22 -0700

    Created stubs and skeletons for windows to display information

---
+++ src/window.py
@@ -0,0 +1,87 @@
+"""
+Handles all windows and displaying of information for flashcard program.
+
+Author: Ethan Posner
+Date: July 27, 2021
+"""
+
+import tkinter as tk
+
+
+class Root(tk.Tk):
+    """
+    Main object which handles all frames (current display of widgets) for window
+    """
+
+    def __init__(self, background, width=600, height=400):
+
+        tk.Tk.__init__(self)
+        self.width = width
+        self.height = height
+        self.configure(bg=background)
+        self.geometry(f"{width}x{height}")
+
+
+class ItemSelectionFrame(tk.Frame):
+    pass
+
+
+class FlashcardTermFrame(tk.Frame):
+    """
+    Display term to memorize, as well as options to go back or repeat card. This frame will follow or be
+    followed by a definition.
+    """
+    pass
+
+
+class FlashcardDefinitionFrame(tk.Frame):
+    """
+    Display definition for term to memorize, as well as options to go back or repeat card. This frame will follow or
+    be followed by a term.
+    """
+    pass
+
+
+class FlashcardFrame(tk.Frame):
+    """
+    Made up of two frames: the term and definition frames. When clicking on the first frame which shows up, the other
+    frame should be shown. When clicking on that frame, the next flashcard should be displayed. There should also
+    be buttons for repeating the flashcard now or later in the list.
+    """
+    pass
+
+
+class LoginFrame(tk.Frame):
+
+    def __init__(self, parent, login_function, back_function, width=600, height=400):
+        self.width = width
+        self.height = height
+        tk.Frame.__init__(self, parent, width=width, height=height, bg="grey25")
+        self.parent = parent
+        self.login_function = login_function
+        self.goto_register_function = back_function
+
+        login_message_label = tk.Label(self, text="Login Failed", fg="grey25", bg="grey25",
+                                     font=('consolas', 10, 'bold'))
+        login_message_label.place(relx=.5, rely=.1, anchor="center")
+
+        login_label = tk.Label(self, text="LOGIN", fg="white", bg="grey25", font=('consolas', 50, 'bold'))
+        login_label.place(relx=.5, rely=.3, anchor="center")
+
+        username_label = tk.Label(self, text="Username", font=('consolas', 10, 'bold'), bg="grey25", fg="white")
+        username_label.place(relx=.5, rely=.45, anchor="center")
+        username_box = tk.Entry(self, width="20")
+        username_box.place(relx=.5, rely=.5, anchor="center")
+
+        password_label = tk.Label(self, text="Password", font=('consolas', 10, 'bold'), bg="grey25", fg="white")
+        password_label.place(relx=.5, rely=.6, anchor="center")
+        password_box = tk.Entry(self, width="20")
+        password_box.place(relx=.5, rely=.65, anchor="center")
+
+        back_button = tk.Button(self, text="register", font=('consolas', 10, 'bold'), bg="white", fg="black",
+                                command=back_function)
+        back_button.place(relx=.45, rely=.85, anchor="center")
+
+        login_button = tk.Button(self, text="login", font=('consolas', 10, 'bold'), bg="white", fg="black",
+                                 command=login_function)
+        login_button.place(relx=.55, rely=.85, anchor="center")
