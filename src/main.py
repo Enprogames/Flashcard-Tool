4af4bc3d34e76e29f05be27cc232d32e81600d62
enprogames <e_posner@outlook.com>
2021-07-27 19:10:19 -0700

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+Handles initialization of all functionality for flashcard program.
+
+Author: Ethan Posner
+Date: July 27, 2021
+"""
+
+from window import Root, ItemSelectionFrame, FlashcardFrame, LoginFrame
+
+root = Root(width=1000, height=600)
+
+root.mainloop()
