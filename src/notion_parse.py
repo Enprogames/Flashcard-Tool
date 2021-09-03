6164c4a0532fb03ed4988877a7d8857be51c62c3
ethan <e_posner@outlook.com>
2021-09-03 16:56:35 -0700

    Added usage of notion api to parse_notion.py. Still doesn't work :(

--- src/notion_parse.py
+++ src/notion_parse.py
@@ -1,12 +1,21 @@
 import requests
 import json
 import os
+from notion.client import NotionClient
 
 # open api_conf.json to get the api key
 with open("src/api_conf.json", "r") as f:
     api_conf_json = json.load(f)
 
 API_KEY = api_conf_json['api_key']
+
+client = NotionClient(token_v2=API_KEY)
+
+page = client.get_block("https://www.notion.so/Charting-and-Technical-Analysis-89391759c30247ea8f09032107fd7946")
+
+print(page.__dict__)
+exit(0)
+
 headers = {"Authorization": f"Bearer {API_KEY}", "Notion-Version": "2021-05-13", "Content-Type": "application/json"}
 page_id = "89391759-c302-47ea-8f09-032107fd7946"
 
